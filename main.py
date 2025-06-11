5b888d307c2a98d82d4c48716316f9de0a211485
mathias-schnell <mathiasschnell@gmail.com>
2025-06-10 23:24:39 -0700

    Added new objects for rendering a player object on the screen and implementing rotation for the player.

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -11,9 +14,20 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                return
+        dt = clock.tick(60) / 1000
         screen.fill((0,0,0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
 if __name__ == "__main__":
