d8faa7df36f7f8be6b5c6bc8ae67f95efe54a553
mathias-schnell <mathiasschnell@gmail.com>
2025-06-10 23:48:06 -0700

    Implemented player movement, forwards and backwards. Also implemented use of containers to handle various updatable and drawable groups.

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
 
     while True:
@@ -24,10 +28,15 @@ def main():
                 pygame.quit()
                 sys.exit()
                 return
+
         dt = clock.tick(60) / 1000
         screen.fill((0,0,0))
-        player.draw(screen)
-        player.update(dt)
+
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
 if __name__ == "__main__":
