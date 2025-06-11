30d4d763ecb87e72404ba36574a59cbbf811adb2
mathias-schnell <mathiasschnell@gmail.com>
2025-06-11 00:36:02 -0700

    Added classes for asteroids and an asteroid field. These are the beginnings of basic flying objects on the game screen for the player to shoot at.

---
+++ asteroid.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -6,37 +6,62 @@ import sys
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
+    # print some initialization statements to console
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # start pygame
     pygame.init()
 
+    # the game screen, clock and time delta
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    # sprite containers
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    asteroids = pygame.sprite.Group()
 
+    # set the containers for each game object
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    # create the player
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+    ast_field = AsteroidField()
+    
+    # the game loop
     while True:
+
+        # check for anything that would close the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
                 return
-
+        
+        # calculate time delta
         dt = clock.tick(60) / 1000
+
+        # black background
         screen.fill((0,0,0))
 
+        # update all the updatable objects
         for obj in updatable:
             obj.update(dt)
+        
+        # draw all the drawable objects
         for obj in drawable:
             obj.draw(screen)
 
+        # refresh the screen
         pygame.display.flip()
 
 if __name__ == "__main__":
